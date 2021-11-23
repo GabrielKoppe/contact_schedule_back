1b5957d220f7ed953f9097f4a56a9bff6d9aabd0
Gabriel Koppe <Gabriel.koppe@gmail.com>
2021-11-23 17:53:17 -0300

    Atualização de Autenticação de Login

--- auth/user_auth.py
+++ auth/user_auth.py
@@ -1,9 +1,13 @@
 import base64
 
 def user_auth(auth):
+    print(auth)
+    if "Basic" in auth:
+        auth = auth.replace("Basic ","")
+    auth = base64.b64decode(auth).decode('UTF-8')
     auth = auth.split(sep=":")
-    name = base64.b64decode(auth[0]).decode('UTF-8')
-    password = base64.b64decode(auth[1]).decode('UTF-8')
+    name = auth[0]
+    password = auth[1]
     body = {
         "name": name,
         "password": password
